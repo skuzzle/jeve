2f19085270fac67ecff9b7a771673383df990fe9
Simon Taddiken <simon.taddiken@gmail.com>
2014-04-08 10:30:13 +0200

    added java doc to new class

--- src/main/java/de/skuzzle/jeve/RegistrationEvent.java
+++ src/main/java/de/skuzzle/jeve/RegistrationEvent.java
@@ -1,11 +1,21 @@
 package de.skuzzle.jeve;
 
-
+/**
+ * RegistrationEvents are created when adding or removing a {@link Listener} from an
+ * {@link EventProvider}.
+ * 
+ * @author Simon Taddiken
+ */
 public class RegistrationEvent extends Event<EventProvider> {
 
+    /** The class for which the listener has been added or removed */
     private final Class<? extends Listener> cls;
     
-    
+    /**
+     * Creates a new RegistrationEvent.
+     * @param source The EventProvider for which a Listener as added or removed.
+     * @param cls The class for which the Listener was added ore removed.
+     */
     RegistrationEvent(EventProvider source, Class<? extends Listener> cls) {
         super(source);
         this.cls = cls;
@@ -13,6 +23,11 @@ public class RegistrationEvent extends Event<EventProvider> {
 
     
     
+    /**
+     * Gets the class for which the Listener has been added or removed.
+     * 
+     * @return The event class.
+     */
     public Class<? extends Listener> getEventClass() {
         return this.cls;
     }

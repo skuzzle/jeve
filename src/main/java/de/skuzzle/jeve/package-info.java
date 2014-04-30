770f9779af677289cd11788769fa3dbfd440ac4c
Simon Taddiken <simon.taddiken@gmail.com>
2014-05-01 00:31:47 +0200

    adapted package info to new version number

--- src/main/java/de/skuzzle/jeve/package-info.java
+++ src/main/java/de/skuzzle/jeve/package-info.java
@@ -1,5 +1,5 @@
 /**
- * <h2>jeve 1.0.0</h2>
+ * <h2>jeve 1.1.0</h2>
  * jeve is an open source framework for implementing the observer pattern in Java 8 . It 
  * makes use of lambda expressions and method references to clarify event delegation code.
  * 
@@ -7,7 +7,7 @@
  * customize it to your needs. Please see the 
  * <a href="https://github.com/skuzzle/jeve">quickstart guide</a> on GitHub to get 
  * started. This documentation can be viewed online 
- * <a href="http://www.jeve.skuzzle.de/1.0.0/doc">here</a>.</p>
+ * <a href="http://www.jeve.skuzzle.de/1.1.0/doc">here</a>.</p>
  * 
  * @author Simon Taddiken
  * @since 1.0.0

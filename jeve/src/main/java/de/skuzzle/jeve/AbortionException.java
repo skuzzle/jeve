d45b37b43360218e939dc7ee2fee79cd875a12e5
Simon Taddiken <simon.taddiken@gmail.com>
2014-06-04 11:56:55 +0200

    some more exception constructors

--- jeve/src/main/java/de/skuzzle/jeve/AbortionException.java
+++ jeve/src/main/java/de/skuzzle/jeve/AbortionException.java
@@ -1,9 +1,9 @@
 package de.skuzzle.jeve;
 
 /**
- * Can be thrown by {@link Listener Listeners} or 
- * {@link ExceptionCallback ExceptionCallbacks} to make the event dispatching explicitly
- * fail with an exception. All other kinds of exceptions will be swallowed by the
+ * Can be thrown by {@link Listener Listeners} or {@link ExceptionCallback
+ * ExceptionCallbacks} to make the event dispatching explicitly fail with an
+ * exception. All other kinds of exceptions will be swallowed by the
  * {@link EventProvider}.
  * 
  * @author Simon Taddiken
@@ -13,14 +13,47 @@ public class AbortionException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
+
+
     /**
      * Creates a new AbortionException
      */
-    public AbortionException() {}
-    
+    public AbortionException() {
+    }
+
+
+
+    /**
+     * Creates a new AbortionException with a message.
+     * 
+     * @param message
+     *            The message
+     */
+    public AbortionException(String message) {
+        super(message);
+    }
+
+
+
+    /**
+     * Creates a new AbortionException with message and a cause
+     * 
+     * @param message
+     *            The message
+     * @param cause
+     *            The cause.
+     */
+    public AbortionException(String message, Exception cause) {
+        super(message, cause);
+    }
+
+
+
     /**
      * Creates a new AbortionException with a cause
-     * @param cause The cause.
+     * 
+     * @param cause
+     *            The cause.
      */
     public AbortionException(Exception cause) {
         super(cause);

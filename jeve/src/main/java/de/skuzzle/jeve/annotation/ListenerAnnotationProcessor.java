65b191b560df2015528a54f1b860f6b91c0bc906
Simon Taddiken <simon.taddiken@gmail.com>
2014-09-04 11:06:48 +0200

    Fix annotation processor

--- jeve/src/main/java/de/skuzzle/jeve/annotation/ListenerAnnotationProcessor.java
+++ jeve/src/main/java/de/skuzzle/jeve/annotation/ListenerAnnotationProcessor.java
@@ -26,60 +26,60 @@ import de.skuzzle.jeve.Listener;
 /**
  * Processes classes which are marked with {@link ListenerInterface} annotation and checks
  * whether they adhere to the general contract of {@link Listener} implementations.
- * 
+ *
  * @author Simon Taddiken
  */
 @SupportedAnnotationTypes("de.skuzzle.jeve.annotation.ListenerInterface")
 public class ListenerAnnotationProcessor extends AbstractProcessor {
 
-    private final static String EXPECTED_TYPE = 
+    private final static String EXPECTED_TYPE =
             "Listening method '%s' must return %s in order to conform to ListenerKind %s";
-    
-    private final static String ILLEGAL_PARAMETER = 
+
+    private final static String ILLEGAL_PARAMETER =
             "Listening method '%s' must have a single parameter of type 'Event'";
-    
-    private final static String ILLEGAL_EXCEPTION = 
+
+    private final static String ILLEGAL_EXCEPTION =
             "Listening method '%s' can not throw checked exception";
-    
-    private final static String EMPTY_LISTENER = 
+
+    private final static String EMPTY_LISTENER =
             "Listener '%s' does not declare any listening methods";
-    
-    private final static String INTERFACE_ONLY = 
+
+    private final static String INTERFACE_ONLY =
             "@ListenerInterface only supported on interface types";
-    
+
     private final static String MISSING_INHERITANCE =
             "@ListenerInterface '%s' must extend de.skuzzle.jeve.Listener";
-    
+
     private final static String TAGGING_NOT_EMPTY = "Tagging listeners must be empty";
-    
-    
-    
+
+
+
     @Override
     public SourceVersion getSupportedSourceVersion() {
         return SourceVersion.latestSupported();
     }
-    
-    
-    
+
+
+
     @Override
     public boolean process(Set<? extends TypeElement> annotations,
             RoundEnvironment roundEnv) {
-        
+
         final Messager msg = this.processingEnv.getMessager();
-        
+
         final Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(
                 ListenerInterface.class);
-        
+
         for (final Element parent : elements) {
             if (parent.getKind() != ElementKind.INTERFACE) {
                 msg.printMessage(Kind.ERROR, INTERFACE_ONLY, parent);
             }
-            
-            this.checkInheritance(parent);
-            
+
+            checkInheritance(parent);
+
             final ListenerInterface anno = parent.getAnnotation(ListenerInterface.class);
             final ListenerKind kind = anno.value();
-        
+
             final List<ExecutableElement> members = parent.getEnclosedElements().stream()
                     .filter(m -> m instanceof ExecutableElement)
                     .map(m -> (ExecutableElement) m)
@@ -94,30 +94,30 @@ public class ListenerAnnotationProcessor extends AbstractProcessor {
                 msg.printMessage(Kind.WARNING, EMPTY_LISTENER, parent);
                 continue;
             }
-            
+
             for (final ExecutableElement member : members) {
-                this.checkReturnValue(member, kind);
-                this.checkParameter(member);
-                this.checkThrown(member);
+                checkReturnValue(member, kind);
+                checkParameter(member);
+                checkThrown(member);
             }
         }
-        
+
         return true;
     }
-    
-    
-    
+
+
+
     private void checkReturnValue(ExecutableElement member, ListenerKind expectedKind) {
         final Types types = this.processingEnv.getTypeUtils();
         final Messager msg = this.processingEnv.getMessager();
-        
+
         final TypeMirror voidPrim = types.getNoType(TypeKind.VOID);
         final TypeMirror ret = member.getReturnType();
-        
+
         switch (expectedKind) {
         case NORMAL:
             if (!types.isSameType(ret, voidPrim)) {
-                msg.printMessage(Kind.ERROR, String.format(EXPECTED_TYPE, 
+                msg.printMessage(Kind.ERROR, String.format(EXPECTED_TYPE,
                         member.getSimpleName(), "'void'", expectedKind), member);
             }
             break;
@@ -126,64 +126,68 @@ public class ListenerAnnotationProcessor extends AbstractProcessor {
             break;
         }
     }
-    
-    
-    
+
+
+
     private void checkInheritance(Element parent) {
         final Messager msg = this.processingEnv.getMessager();
         final Types types = this.processingEnv.getTypeUtils();
         final Elements elements = this.processingEnv.getElementUtils();
-        
+
         final TypeMirror listenerType = elements.getTypeElement(
                 Listener.class.getName()).asType();
         if (!types.isSubtype(parent.asType(), listenerType)) {
-            msg.printMessage(Kind.ERROR, 
+            msg.printMessage(Kind.ERROR,
                     String.format(MISSING_INHERITANCE, parent.getSimpleName()), parent);
         }
     }
-    
-    
-    
+
+
+
     private void checkParameter(ExecutableElement member) {
         final Types types = this.processingEnv.getTypeUtils();
         final Messager msg = this.processingEnv.getMessager();
-        
-        
+
+
         final List<? extends VariableElement> params = member.getParameters();
-        
+
         if (params.size() != 1) {
-            msg.printMessage(Kind.ERROR, String.format(ILLEGAL_PARAMETER, 
+            msg.printMessage(Kind.ERROR, String.format(ILLEGAL_PARAMETER,
                     member.getSimpleName()), member);
             return;
         }
-        
-        final VariableElement param = params.iterator().next();
-        final TypeElement eventType = processingEnv.getElementUtils().getTypeElement(
-                Event.class.getName());
 
-        final TypeMirror eType = types.getDeclaredType(eventType, 
-                types.getWildcardType(null, null));
+        final Elements elements = this.processingEnv.getElementUtils();
+        final VariableElement param = params.iterator().next();
+        final TypeElement eventType = elements.getTypeElement(Event.class.getName());
+
+        final TypeElement listenerType = elements.getTypeElement(
+                Listener.class.getName());
+        final TypeMirror listenerMirror = types.getDeclaredType(listenerType);
+        final TypeMirror eType = types.getDeclaredType(eventType,
+                types.getWildcardType(null, null),
+                types.getWildcardType(listenerMirror, null));
         if (!types.isSubtype(param.asType(), eType)) {
-            msg.printMessage(Kind.ERROR, String.format(ILLEGAL_PARAMETER, 
+            msg.printMessage(Kind.ERROR, String.format(ILLEGAL_PARAMETER,
                     member.getSimpleName()), param);
         }
     }
-    
-    
-    
+
+
+
     private void checkThrown(ExecutableElement member) {
         final Types types = this.processingEnv.getTypeUtils();
         final Messager msg = this.processingEnv.getMessager();
-        
+
         final List<? extends TypeMirror> thrown = member.getThrownTypes();
         final TypeMirror runtimeEx = this.processingEnv.getElementUtils().getTypeElement(
                 RuntimeException.class.getName()).asType();
-        
+
         for (final TypeMirror tm : thrown) {
             if (!types.isSubtype(tm, runtimeEx)) {
-                msg.printMessage(Kind.ERROR, 
+                msg.printMessage(Kind.ERROR,
                         String.format(ILLEGAL_EXCEPTION, member.getSimpleName()), member);
-                
+
                 // only check one exception
                 return;
             }

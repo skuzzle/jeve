5a0b454e864490761fb1050fbaa1e0541b916080
Simon Taddiken <simon.taddiken@gmail.com>
2015-05-13 19:02:13 +0200

    Rename class

--- jeve/src/main/java/de/skuzzle/jeve/FailureCollector.java
+++ jeve/src/main/java/de/skuzzle/jeve/FailureCollector.java
@@ -9,51 +9,64 @@ import de.skuzzle.jeve.invoke.FailedEventInvocation;
 
 /**
  * This class can be used for collecting failed dispatch attempts during event
- * dispatching. This might preferably be used for debugging purposes to locally collect
- * all failed invocations:
+ * dispatching. This might preferably be used for debugging purposes to locally
+ * collect all failed invocations:
  *
  * <pre>
- * FailedEventsCollector collector = FailedEventsCollecter.create();
+ * FailureCollector collector = FailedEventsCollecter.create();
  * eventProvider.dispatch(myEvent, MyListener::handle, collector);
  *
- * for (EventInvocation&lt;?,?&gt; failed : collector) {
+ * for (EventInvocation&lt;?, ?&gt; failed : collector) {
  *     // ...
  * }
  * </pre>
  *
- * If you need to notify another ExceptionCallback, you can use the second factory
- * method. The following will collect all failed invocations and also delegate them to
- * the given other ExceptionCallback.
+ * If you need to notify another ExceptionCallback, you can use the second
+ * factory method. The following will collect all failed invocations and also
+ * delegate them to the given other ExceptionCallback.
  *
  * <pre>
  * ExceptionCallback delegate = ...;
- * FailedEventsCollector collector = FailedEvents.delegatingTo(delegate);
+ * FailureCollector collector = FailedEvents.delegatingTo(delegate);
  * ...
  * </pre>
  *
  * @author Simon Taddiken
  * @since 3.0.0
  */
-public final class FailedEventsCollector implements ExceptionCallback,
+public final class FailureCollector implements ExceptionCallback,
         Iterable<FailedEventInvocation> {
 
     private final Object mutex = new Object();
     private List<FailedEventInvocation> failedEvents;
     private final ExceptionCallback delegate;
 
-    private FailedEventsCollector(ExceptionCallback delegate) {
+    private FailureCollector(ExceptionCallback delegate) {
         this.delegate = delegate;
     }
 
-    public static FailedEventsCollector delegatingTo(ExceptionCallback ec) {
+    /**
+     * Creates a new FailureCollector which collects every failed notification
+     * attempt, then handle the exception with the given delegate
+     * {@link ExceptionCallback}.
+     *
+     * @param ec The delegate.
+     * @return The new FailureCollector.
+     */
+    public static FailureCollector delegatingTo(ExceptionCallback ec) {
         if (ec == null) {
             throw new IllegalArgumentException("ec is null");
         }
-        return new FailedEventsCollector(ec);
+        return new FailureCollector(ec);
     }
 
-    public static FailedEventsCollector create() {
-        return new FailedEventsCollector(null);
+    /**
+     * Creates a new FailureCollector.
+     *
+     * @return The new FailureCollector.
+     */
+    public static FailureCollector create() {
+        return new FailureCollector(null);
     }
 
     public List<FailedEventInvocation> getFailedInvocations() {
